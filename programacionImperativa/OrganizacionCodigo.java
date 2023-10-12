3d51a062bd808296749cc40901221d672afa9b5a
flor-galarza <florencia.galarzamaumary@gmail.com>
2023-10-12 00:20:49 -0300

    Cambios

---
+++ programacionImperativa/OrganizacionCodigo.java
@@ -0,0 +1,24 @@
+package programacionImperativa;
+
+public class OrganizacionCodigo {
+    public static void main(String[] args) {
+
+        // declaracion
+        int variable;
+
+        // asignacion
+        variable = 10;
+
+        // declaro y asigno
+        int var2 = 5;
+
+        // -----------Tipos de Datos--------------
+
+        //Byte
+        
+
+        System.out.println("Rango del valor de byte: " + Byte.MIN_VALUE + " "+ Byte.MAX_VALUE);
+
+    }
+
+}
